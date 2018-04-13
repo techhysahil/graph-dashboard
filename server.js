346005fb195aeb6597ec2b13990384ce394d2094
sahil.gu <sahil.gu@MMMCDHONEYT.local>
2018-04-13 05:37:25 +0530

    server changes

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const { renderToString } = require('react-dom/server');
 
 const server = express();
 
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 server.get('/', (req, res) => {
   const htmlPath = path.resolve(__dirname, 'build', 'index.html');
 
@@ -15,7 +21,6 @@ server.get('/', (req, res) => {
     const renderedApp = renderToString(React.createElement(App, null));
 
     res.send(html.replace(rootElem, rootElem + renderedApp));
-    res.send()
   });
 });
 
